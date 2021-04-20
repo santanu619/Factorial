9659af8824cd9d3d83a1ae97039fad3e72170969
santanu619 <mohapatra.santanu123@gmail.com>
2021-04-20 17:20:20 +0530

    Added: Code of Factorials

---
+++ Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int fact = 1;
+            int n;
+            Console.WriteLine("Enter the Number:");
+            n = int.Parse(Console.ReadLine());
+            for(i=1;i<=n;i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial is:"+fact);
+        }
+    }
+}
